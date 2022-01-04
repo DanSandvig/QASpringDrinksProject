5987f247340851facd3e07c9c100f9efd89876d8
DanSandvig <dspublicaddress@protonmail.com>
2022-01-04 10:49:32 +0000

    Initial commit, basic Spring Boot starter files

---
+++ src/main/java/com/sandvig/qaspringdrinksproject/QaSpringDrinksProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sandvig.qaspringdrinksproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaSpringDrinksProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaSpringDrinksProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandvig/qaspringdrinksproject/QaSpringDrinksProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandvig.qaspringdrinksproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaSpringDrinksProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
