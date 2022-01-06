cdb0315451ba3e62c531e5bbbcffc7543c516334
DanSandvig <dspublicaddress@protonmail.com>
2022-01-06 12:27:52 +0000

    Completed first unit test for controller

---
+++ src/test/java/com/sandvig/qaspringdrinksproject/controller/DrinkControllerUnitTests.java
@@ -0,0 +1,48 @@
+package com.sandvig.qaspringdrinksproject.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sandvig.qaspringdrinksproject.domain.Drink;
+import com.sandvig.qaspringdrinksproject.service.DrinkService;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class DrinkControllerUnitTests {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	@MockBean
+	private DrinkService drinkService;
+	
+	@Test
+	public void createTest() throws Exception {
+		Drink testDrink = new Drink("Tizer", 1000, true, 333);
+		String testDrinkAsJson = this.mapper.writeValueAsString(testDrink);
+		
+		Mockito.when(this.drinkService.create(testDrink)).thenReturn(testDrink);
+		
+		mvc.perform(post("/drink/create")
+		.content(testDrinkAsJson)
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isCreated())
+		.andExpect(content().json(testDrinkAsJson));
+	}
+
+}
